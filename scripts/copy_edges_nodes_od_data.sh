15ca9d1bd308928af083640dec08d447601e27ad
Carlos Perez <perez@doorstep.com>
2018-03-17 14:15:50 -0600

    Add full edges/nodes/od create scripts

---
+++ scripts/copy_edges_nodes_od_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Copies the Edges, Nodes, and OD full datasets
+#
+STRAVA_ROOT=/mnt/strava_metro/co_201701_201712_ride
+pushd $STRAVA_ROOT/Edges
+psql -U cperez -h strava-metro-db1.cjgeimtqkdes.us-west-2.rds.amazonaws.com -d strava_metro -c "\COPY colorado_osm_edges_metro_edges_data FROM 'colorado_osm_edges_metro_edges_data.csv' DELIMITERS ',' CSV HEADER;"
+popd
+pushd $STRAVA_ROOT/Nodes
+psql -U cperez -h strava-metro-db1.cjgeimtqkdes.us-west-2.rds.amazonaws.com -d strava_metro -c "\COPY colorado_osm_edges_metro_nodes_data FROM 'colorado_osm_edges_metro_nodes_data.csv' DELIMITERS ',' CSV HEADER;"
+popd
+pushd $STRAVA_ROOT/OD
+psql -U cperez -h strava-metro-db1.cjgeimtqkdes.us-west-2.rds.amazonaws.com -d strava_metro -c "\COPY colorado_osm_edges_metro_od_data FROM 'colorado_osm_edges_metro_od_data.csv' DELIMITERS ',' CSV HEADER;"
+popd
