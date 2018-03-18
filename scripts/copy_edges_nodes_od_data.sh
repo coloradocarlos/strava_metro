98bf18c93ab4706c1454b087be37ddd087864a8f
Carlos Perez <perez@doorstep.com>
2018-03-17 19:40:13 -0600

    Change user and host to env variables

--- scripts/copy_edges_nodes_od_data.sh
+++ scripts/copy_edges_nodes_od_data.sh
@@ -4,11 +4,11 @@
 #
 STRAVA_ROOT=/mnt/strava_metro/co_201701_201712_ride
 pushd $STRAVA_ROOT/Edges
-psql -U cperez -h strava-metro-db1.cjgeimtqkdes.us-west-2.rds.amazonaws.com -d strava_metro -c "\COPY colorado_osm_edges_metro_edges_data FROM 'edges_data.csv' DELIMITERS ',' CSV HEADER;"
+psql -U $RUSER -h $RHOST -d strava_metro -c "\COPY colorado_osm_edges_metro_edges_data FROM 'edges_data.csv' DELIMITERS ',' CSV HEADER;"
 popd
 pushd $STRAVA_ROOT/Nodes
-psql -U cperez -h strava-metro-db1.cjgeimtqkdes.us-west-2.rds.amazonaws.com -d strava_metro -c "\COPY colorado_osm_edges_metro_nodes_data FROM 'nodes_data.csv' DELIMITERS ',' CSV HEADER;"
+psql -U $RUSER -h $RHOST -d strava_metro -c "\COPY colorado_osm_edges_metro_nodes_data FROM 'nodes_data.csv' DELIMITERS ',' CSV HEADER;"
 popd
 pushd $STRAVA_ROOT/OD
-psql -U cperez -h strava-metro-db1.cjgeimtqkdes.us-west-2.rds.amazonaws.com -d strava_metro -c "\COPY colorado_osm_edges_metro_od_data FROM 'od_data.csv' DELIMITERS ',' CSV HEADER;"
+psql -U $RUSER -h $RHOST -d strava_metro -c "\COPY colorado_osm_edges_metro_od_data FROM 'od_data.csv' DELIMITERS ',' CSV HEADER;"
 popd
