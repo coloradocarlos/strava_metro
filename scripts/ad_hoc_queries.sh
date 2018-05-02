a135499d6bb7b56ff6791c9faace4784f97c0fbd
Carlos Perez <perez@doorstep.com>
2018-05-02 09:13:12 -0600

    Improve script for exporting to CSV

--- scripts/ad_hoc_queries.sh
+++ scripts/ad_hoc_queries.sh
@@ -5,4 +5,25 @@
 # 26th Street ZELT, all 2017, all 2017 by month, 22223
 #
 
-psql -U $RUSER -h $RHOST -d strava_metro -c "\COPY (SELECT id, osm_id, osm_name, rollup_date, tactcnt, tactcnt_0, tactcnt_1, tactcnt_2, tactcnt_3, tactcnt_4, tactcnt_5, tactcnt_6, tactcnt_7 FROM cos_osm_edges_rollup_weekday WHERE id=22223 ORDER BY rollup_date) TO '26th_st_zelt.csv' WITH CSV HEADER;"
+WEEK=weekday
+
+EDGE_ID=22223
+CSV_FILE=26th_st_zelt
+
+# EDGE_ID=407672
+# CSV_FILE=rock_island_trail_1
+
+# EDGE_ID=484465
+# CSV_FILE=rock_island_trail_2
+
+# EDGE_ID=361108
+# CSV_FILE=templeton_gap
+
+# EDGE_ID=16892
+# CSV_FILE=tejon_st
+
+# EDGE_ID=821836
+# CSV_FILE=midland_trail
+
+if [ ! -d 'csv' ]; then mkdir 'csv'; fi
+psql -U $RUSER -h $RHOST -d strava_metro -c "\COPY (SELECT id, osm_id, osm_name, rollup_date, tactcnt, tactcnt_0, tactcnt_1, tactcnt_2, tactcnt_3, tactcnt_4, tactcnt_5, tactcnt_6, tactcnt_7 FROM cos_osm_edges_rollup_$WEEK WHERE id=$EDGE_ID ORDER BY rollup_date) TO './csv/$CSV_FILE.$WEEK.csv' WITH CSV HEADER;"
--- sql/create_edges_rollup_view.sql
+++ sql/create_edges_rollup_view.sql
@@ -9,7 +9,7 @@
 DROP MATERIALIZED VIEW IF EXISTS cos_osm_edges_rollup_total;
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS cos_osm_edges_rollup_total
-AS SELECT * FROM cos_osm_edges AS e 
+AS SELECT * FROM cos_osm_edges AS e
 LEFT JOIN (
 	SELECT '2017_01' AS rollup_date, * FROM ride_rollup_month_2017_1_total
 	UNION ALL SELECT '2017_02' as rollup_date, * from ride_rollup_month_2017_2_total
@@ -32,7 +32,7 @@ LEFT JOIN (
 DROP MATERIALIZED VIEW IF EXISTS cos_osm_edges_rollup_weekend;
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS cos_osm_edges_rollup_weekend
-AS SELECT * FROM cos_osm_edges AS e 
+AS SELECT * FROM cos_osm_edges AS e
 LEFT JOIN (
 	SELECT '2017_01' AS rollup_date, * FROM ride_rollup_month_2017_1_weekend
 	UNION ALL SELECT '2017_02' as rollup_date, * from ride_rollup_month_2017_2_weekend
@@ -55,9 +55,9 @@ LEFT JOIN (
 DROP MATERIALIZED VIEW IF EXISTS cos_osm_edges_rollup_weekday;
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS cos_osm_edges_rollup_weekday
-AS SELECT * FROM cos_osm_edges AS e 
+AS SELECT * FROM cos_osm_edges AS e
 LEFT JOIN (
-	SELECT '2017_01' AS rollup_date, * FROM ride_rollup_month_2017_1_weekend
+	SELECT '2017_01' AS rollup_date, * FROM ride_rollup_month_2017_1_weekday
 	UNION ALL SELECT '2017_02' as rollup_date, * from ride_rollup_month_2017_2_weekday
 	UNION ALL SELECT '2017_03' as rollup_date, * from ride_rollup_month_2017_3_weekday
 	UNION ALL SELECT '2017_04' as rollup_date, * from ride_rollup_month_2017_4_weekday
